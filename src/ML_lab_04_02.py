7444dd66cfd211456dd61d10275dd03e3ab53986
chengbin <sbkim0407@gmail.com>
2018-12-17 10:13:58 +0900

    add ML_lab_04_02.py & ML_lab_04_03.py

---
+++ src/ML_lab_04_02.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+tf.set_random_seed(777)  # for reproducibility
+
+xy = np.loadtxt('../data/data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+# Make sure the shape and data are OK
+print(x_data.shape, x_data, len(x_data))
+print(y_data.shape, y_data)
+
+# placeholders for a tensor that will be always fed
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis
+hypothesis = tf.matmul(X, W) + b
+
+# Simplified cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train_op = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+# Set up feed_dict variables inside the loop.
+for step in range(2001):
+    cost_val, hy_val, _ = sess.run([cost, hypothesis, train_op], feed_dict={X: x_data, Y: y_data})
+
+    if step % 10 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+
+# Ask my score
+print("Your score will be: ", sess.run(hypothesis, feed_dict={X: [[100, 70, 101]]}))
+print("Other scores will be: ", sess.run(hypothesis, feed_dict={X: [[60, 70, 110], [90, 100, 80]]}))
